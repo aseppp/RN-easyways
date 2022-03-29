89cb3e117a073942db8e41003bd66463ce02865d
aseppp <saepudinasep86@gmail.com>
2022-03-29 18:53:34 +0700

    checkpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const {
   getTodos,
 } = require("../controller/todos");
 router.post("/addTodo", addTodo);
-router.patch("/updateTodo/:id", editTodo);
+router.put("/updateTodo/:id", editTodo);
 router.delete("/delete/:id", deleteTodo);
 router.get("/todos", getTodos);
 
